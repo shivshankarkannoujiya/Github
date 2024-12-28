5ef505e21573298a9aa8b6db97e10d8e3529b632
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-29 02:29:27 +0530

    feat: implement getAllIssues functionality with error handling and response messages

--- Backend/src/controllers/issues.controllers.js
+++ Backend/src/controllers/issues.controllers.js
@@ -32,7 +32,29 @@ const createIssue = async (req, res) => {
     }
 };
 
-const getAllIssues = async (req, res) => {};
+const getAllIssues = async (req, res) => {
+    try {
+        const issues = await Issue.find({});
+        if (issues.length === 0) {
+            return res.status(404).json({
+                message: 'No Issue found',
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Issues fetched successfully',
+            Issues: issues,
+        });
+    } catch (error) {
+        console.error('Error fetching Issues', error);
+        return res.status(500).json({
+            message: 'An error occured while fetching Issues',
+            error: error.message,
+        });
+    }
+};
+
+
 const getIssueById = async (req, res) => {};
 
 const updateIssueById = async (req, res) => {};
