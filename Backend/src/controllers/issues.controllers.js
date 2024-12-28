b60a4ed60b432a9169de4f65bebd3b4ed0b829d4
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-29 02:25:22 +0530

    feat: implement create issue functionality with validation and error handling

--- Backend/src/controllers/issues.controllers.js
+++ Backend/src/controllers/issues.controllers.js
@@ -1,4 +1,37 @@
-const createIssue = async (req, res) => {};
+import { Issue } from '../models/issues.models.js';
+
+const createIssue = async (req, res) => {
+    const { title, description } = req.body;
+    const repoId = req.params.id;
+
+    try {
+        console.log('RepoID: ', repoId);
+
+        if (!title || !description) {
+            return res.status(411).json({
+                message: 'All fields are required',
+            });
+        }
+
+        const newIssue = Issue.create({
+            title,
+            description,
+            repository: repoId,
+        });
+
+        return res.status(201).json({
+            Issue: newIssue,
+            message: 'Issue created successfully',
+        });
+    } catch (error) {
+        console.error('Error creating Issue: ', error);
+        return res.status(500).json({
+            message: 'An error occured while creating issue',
+            error: error.message,
+        });
+    }
+};
+
 const getAllIssues = async (req, res) => {};
 const getIssueById = async (req, res) => {};
 
