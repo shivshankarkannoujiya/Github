ddd572d7b09940499322e4f7c5455dc40cde5014
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-29 02:47:50 +0530

    feat: implement getIssueById, updateIssueById, and deleteIssueById functionalities with error handling and response messages

--- Backend/src/controllers/issues.controllers.js
+++ Backend/src/controllers/issues.controllers.js
@@ -54,11 +54,86 @@ const getAllIssues = async (req, res) => {
     }
 };
 
+const getIssueById = async (req, res) => {
+    const issueId = req.params.id;
+    try {
+        const issue = await Issue.findById(issueId);
+        if (!issue) {
+            return res.status(404).json({
+                message: 'issue not found',
+            });
+        }
 
-const getIssueById = async (req, res) => {};
+        return res.status(200).json({
+            message: 'Issue fetched successfully',
+            issue: issue,
+        });
+    } catch (error) {
+        console.error('Error fetching issue by id: ', error);
+        return res.status(500).json({
+            message: 'An occured while fetching issue by id',
+            error: error.message,
+        });
+    }
+};
 
-const updateIssueById = async (req, res) => {};
-const deleteIssueById = async (req, res) => {};
+const updateIssueById = async (req, res) => {
+    const { id } = req.params;
+    const { title, description, status } = req.body;
+
+    try {
+        const updatedIssue = await Issue.findByIdAndUpdate(
+            id,
+            {
+                $set: {
+                    title: title,
+                    description: description,
+                    status: status,
+                },
+            },
+            { new: true }
+        );
+        if (!updatedIssue) {
+            return res.status(404).json({
+                message: 'issue not found',
+            });
+        }
+
+        return res.status(200).json({
+            message: 'Isuue updated successfully',
+            issue: updatedIssue,
+        });
+    } catch (error) {
+        console.error('Error updating issue', error);
+        return res.status(500).json({
+            message: 'An error occured while updating issue',
+            error: error.message,
+        });
+    }
+};
+const deleteIssueById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const deletedIssue = await Issue.findByIdAndDelete(id);
+        if (!deletedIssue) {
+            return res.status(404).json({
+                message: 'Issue not found',
+            });
+        }
+
+        return res.status(204).json({
+            message: 'Issue deleted successfully',
+            issue: deletedIssue,
+        });
+    } catch (error) {
+        console.error('Error deleting Issue');
+        return res.status(500).json({
+            message: 'An error occured while deleting Issue',
+            error: error.message,
+        });
+    }
+};
 
 export {
     createIssue,
